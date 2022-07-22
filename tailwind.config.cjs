ffd09f129a45ad079ba487bc3c16b237f2fb250d
naneps <ekaprasetya2244@gmail.com>
2022-07-22 21:44:43 +0700

    custom theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,13 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
-    
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#a17fff",
+
+          secondary: "#7a6cd1",
+
+          accent: "#f4024b",
+
+          neutral: "#2B232E",
+
+          "base-100": "#2F2D43",
+
+          info: "#2D74D7",
+
+          success: "#74ECB2",
+
+          warning: "#BA9D0D",
+
+          error: "#FC5331",
+        },
+      },
+    ],
   },
   plugins: [require("daisyui")],
-}
+};
